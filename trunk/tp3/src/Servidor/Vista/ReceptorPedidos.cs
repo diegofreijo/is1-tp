52ef7b87fb53c19e1682cec0edcd205cbd6aa713
giga.freijo <giga.freijo@01d69df5-974a-0410-b7b9-c36925a45e4d>
2008-06-23 02:59:05 +0000

    Comence a remodelar el diseño de la forma de entrada de pedidos y salida de respuestas. Comence con el codigo de esas cosas.

---
+++ trunk/tp3/src/Servidor/Vista/ReceptorPedidos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CasinoOnline.Servidor.Vista
+{
+    abstract class ReceptorPedidos
+    {
+        /// <summary>
+        /// Comienza a recibir los pedidos de los clientes
+        /// </summary>
+        public abstract void ComenzarRecepcion();
+    }
+}
