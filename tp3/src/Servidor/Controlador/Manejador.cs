4b56859c6cb29a2dee2b3441ec405ca4ebe1d306
giga.freijo <giga.freijo@01d69df5-974a-0410-b7b9-c36925a45e4d>
2008-06-25 03:52:39 +0000

    Comence a atacar el Controlador.

---
+++ tp3/src/Servidor/Controlador/Manejador.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CasinoOnline.Servidor.Controlador
+{
+	abstract class Manejador
+	{
+		#region Singleton
+		/// <summary>
+		/// La instancia de la clase
+		/// </summary>
+		private static Manejador instancia = null;
+
+		/// <summary>
+		/// Constructor privado para no permitir crear mas instancias que las necesarias
+		/// </summary>
+		private Manejador()
+		{
+			// Asocio el manejador a su tipo de pedido
+			Asociarse();
+		}
+
+		/// <summary>
+		/// Devuelve la instancia de la clase
+		/// </summary>
+		public static Manejador ObtenerInstancia()
+		{
+			if (instancia == null)
+			{
+				instancia = new this.GetType();
+			}
+			return instancia;
+		}
+		#endregion
+	
+		/// <summary>
+		/// Realiza las acciones necesarias para el pedido pasado
+		/// </summary>
+		public abstract void ManejarPedido(Vista.Pedido pedido);
+
+		/// <summary>
+		/// Asocia al manejador con su tipo de pedido
+		/// </summary>
+		private void Asociarse()
+		{
+			DespachadorPedidos.ObtenerInstancia().AsociarManejador(this.PedidoAsociado, this);
+		}
+
+		/// <summary>
+		/// Devuelve el tipo de pedido que tiene asociado este manejador
+		/// </summary>
+		protected abstract Vista.Pedido PedidoAsociado
+		{
+			get;
+		}
+	}
+}
--- tp3/src/Servidor/Program.cs
+++ tp3/src/Servidor/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CasinoOnline.Servidor.Vista;
+using CasinoOnline.Servidor.Controlador;
 
 namespace CasinoOnline.Servidor
 {
@@ -10,9 +11,22 @@ namespace CasinoOnline.Servidor
     {
         static void Main(string[] args)
         {
-            // Comienzo la recepcion de pedidos
-            ReceptorPedidos receptor = new ReceptorPedidosXml();
-            receptor.ComenzarRecepcion();
+			EncenderServidor();
         }
+
+		private static void EncenderServidor()
+		{
+			// Pido que se inicialicen todos los manejadores
+			InicializarManejadores();
+
+			// Comienzo la recepcion de pedidos
+			ReceptorPedidos receptor = new ReceptorPedidosXml();
+			receptor.ComenzarRecepcion();
+		}
+
+		private static void InicializarManejadores()
+		{
+			CasinoOnline.Servidor.Controlador.Manejadores.EntrarCasino.ObtenerInstancia();
+		}
     }
 }
